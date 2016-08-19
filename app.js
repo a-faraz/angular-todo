468284e959498ff7b76c5650e4e047ce861a61c7
Anjum Faraz <faraz@Farazs-MacBook-Pro.local>
2016-08-19 12:13:04 -0500

    implemented Main Controller in app

---
+++ app.js
@@ -0,0 +1,13 @@
+angular.module('app', [])
+
+.controller('MainController', function($scope){
+	$scope.todos = [];
+	$scope.addTodo = function(todo){
+		$scope.todos.push({ title: todo, due: Date.now() })
+		$scope.newTodo = '';
+	};
+
+	$scope.done = function(index) {
+		$scope.todos.splice(index, 1);
+	}
+})
